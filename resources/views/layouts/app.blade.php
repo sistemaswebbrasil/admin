f8f12a131d6ab19a9d7185621b5126d765a6ad07
sistemawebbrasil <adriano.faria@outlook.com.br>
2017-05-21 15:41:31 -0300

    Acabou por hoje

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
                     <!-- Left Side Of Navbar -->
                     <ul class="nav navbar-nav">
+                    <li><a href="{{ route('administracao') }}">Administração</a></li>
                         &nbsp;
                     </ul>
 
